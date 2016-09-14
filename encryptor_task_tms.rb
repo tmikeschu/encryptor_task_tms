d0e6b204b2ffce5e80498b8340d3859a3fe7fe01
Mike Schutte <tmikeschutte@gmail.com>
2016-09-14 13:38:01 -0600

    Update hash, add encrypt methods

--- encryptor_task_tms.rb
+++ encryptor_task_tms.rb
@@ -1,13 +1,21 @@
 class Encryptor
     def cipher 
-    {   'a': 'n', 'b': 'o', 'c': 'p', 
-        'd': 'q', 'e': 'r', 'f': 's', 
-        'g': 't', 'h': 'u', 'i': 'v', 
-        'j': 'w', 'k': 'x', 'l': 'y', 
-        'm': 'z', 'n': 'a', 'o': 'b', 
-        'p': 'c', 'q': 'd', 'r': 'e', 
-        's': 'f', 't': 'g', 'u': 'h',
-        'v': 'i', 'w': 'j', 'x': 'k',
-        'y': 'l', 'z': 'm'}
+    {   'a' => 'n', 'b' => 'o', 'c' => 'p', 
+        'd' => 'q', 'e' => 'r', 'f' => 's', 
+        'g' => 't', 'h' => 'u', 'i' => 'v', 
+        'j' => 'w', 'k' => 'x', 'l' => 'y', 
+        'm' => 'z', 'n' => 'a', 'o' => 'b', 
+        'p' => 'c', 'q' => 'd', 'r' => 'e', 
+        's' => 'f', 't' => 'g', 'u' => 'h',
+        'v' => 'i', 'w' => 'j', 'x' => 'k',
+        'y' => 'l', 'z' => 'm'}
+    end
+    def encrypt_letter(letter) 
+        cipher[letter.downcase]
+    end
+    def encrypt(string)
+        letters = string.split("")
+        post_encryption = letters.collect{ |l| cipher[l.downcase] }
+        post_encryption.join
     end
 end
