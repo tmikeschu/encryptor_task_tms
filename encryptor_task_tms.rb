651d439407e31675267ce9775899f6427a55c51c
Mike Schutte <tmikeschutte@gmail.com>
2016-09-14 13:15:46 -0600

    Add ROT13 cipher hash

--- encryptor_task_tms.rb
+++ encryptor_task_tms.rb
@@ -1,1 +1,13 @@
-puts "hello"
+class Encryptor
+    def cipher 
+    {   'a': 'n', 'b': 'o', 'c': 'p', 
+        'd': 'q', 'e': 'r', 'f': 's', 
+        'g': 't', 'h': 'u', 'i': 'v', 
+        'j': 'w', 'k': 'x', 'l': 'y', 
+        'm': 'z', 'n': 'a', 'o': 'b', 
+        'p': 'c', 'q': 'd', 'r': 'e', 
+        's': 'f', 't': 'g', 'u': 'h',
+        'v': 'i', 'w': 'j', 'x': 'k',
+        'y': 'l', 'z': 'm'}
+    end
+end
